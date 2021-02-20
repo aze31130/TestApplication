5310612e931e7fda31fa4474269f13cfd8209eab
Guilhem <guilhem.carlet@epita.fr>
2021-02-20 16:14:32 +0100

    Added Item class and controller

---
+++ Controllers/ItemController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace WebApplication1.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ItemController : ControllerBase
+    {
+        public static string[] itemNames = { "The Jingling Jester", "Nepta Flowrider", "Singularity" };
+        public static string[] itemDescriptions = { "Fabled Item", "Legendary Item", "Mythical Item" };
+        public static int[] itemPower = { 15, 33, 75 };
+        public static int itemAmount = 3;
+        public static List<Items> CreateItems()
+        {
+            List<Items> items = new List<Items>();
+            for (int i = 0; i < itemAmount; i++)
+            {
+                items.Add(new Items(itemNames[i], itemDescriptions[i], itemPower[i]));
+            }
+            return items;
+        }
+
+        [HttpGet]
+        public IEnumerable<Items> GetStudents()
+        {
+            return CreateItems();
+        }
+
+        [HttpPost]
+        public string PostItem(string message)
+        {
+            /*Items jsonString = JsonSerializer.Deserialize<Items>(item);
+            itemNames.Append(item.name);
+            itemDescriptions.Append(item.description);
+            itemPower.Append(item.power);
+            itemAmount++;
+            return CreateItems();*/
+            return "Hey, you just posted: " + message;
+        }
+
+        [HttpPut("{name}")]
+        public string PutItem(string item)
+        {
+            return "Hey, you just puted: " + item;
+        }
+
+        [HttpGet("{name}")]
+        public ActionResult<Items> GetItemById(string name)
+        {
+            var item = CreateItems().SingleOrDefault(x => x.name == name);
+            if (item != null)
+            {
+                return item;
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+    }
+}
---
+++ Models/Items.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class Items
+    {
+        public Items(string _name, string _description, int _power)
+        {
+            name = _name;
+            description = _description;
+            power = _power;
+        }
+
+        public string name { get; set; }
+        public string description { get; set; }
+        public int power { get; set; }
+
+    }
+}
